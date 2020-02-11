b0096c9ad7bde01d8ed6600b1e79a64eff5764fb
Johannes Konings <johannes.konings@outlook.com>
2020-02-11 21:42:52 +0100

    Player get graphql data

--- src/views/Player/Player.js
+++ src/views/Player/Player.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 // @material-ui/core components
 import { makeStyles } from "@material-ui/core/styles";
 // core components
@@ -8,6 +8,13 @@ import Table from "components/Table/Table.js";
 import Card from "components/Card/Card.js";
 import CardHeader from "components/Card/CardHeader.js";
 import CardBody from "components/Card/CardBody.js";
+
+import Amplify, { API, graphqlOperation } from "aws-amplify";
+import awsconfig from "./../../aws-exports";
+
+import { listPlayers } from "./../../graphql/queries";
+
+Amplify.configure(awsconfig);
 
 const styles = {
   cardCategoryWhite: {
@@ -41,68 +48,49 @@ const styles = {
 
 const useStyles = makeStyles(styles);
 
-export default function Player() {
+//https://dev.to/joemsak/barebones-aws-amplify-react-graphql-app-5ae8
+function Player() {
+  const [playerItems, setPlayerItems] = useState([]);
+
+  const readPlayers = async () => {
+    const allPlayers = await API.graphql(graphqlOperation(listPlayers));
+    console.log(allPlayers.data.listPlayers.items);
+
+    const allPlayerItems = allPlayers.data.listPlayers.items;
+
+    allPlayerItems.sort(function(a, b) {
+      return a.id - b.id;
+    });
+
+    const tableArray = allPlayerItems.map(item => {
+      console.log(item.id, item.name);
+      return [item.id, item.name];
+    });
+
+    setPlayerItems(tableArray);
+  };
+
+  useEffect(() => {
+    readPlayers();
+  }, []);
+
   const classes = useStyles();
+
   return (
     <GridContainer>
       <GridItem xs={12} sm={12} md={12}>
         <Card>
           <CardHeader color="primary">
-            <h4 className={classes.cardTitleWhite}>Simple Table</h4>
+            <h4 className={classes.cardTitleWhite}>Player</h4>
             <p className={classes.cardCategoryWhite}>
-              Here is a subtitle for this table
+              Everyone who can play a game
             </p>
           </CardHeader>
           <CardBody>
             <Table
               tableHeaderColor="primary"
-              tableHead={["Name", "Country", "City", "Salary"]}
-              tableData={[
-                ["Dakota Rice", "Niger", "Oud-Turnhout", "$36,738"],
-                ["Minerva Hooper", "Curaçao", "Sinaai-Waas", "$23,789"],
-                ["Sage Rodriguez", "Netherlands", "Baileux", "$56,142"],
-                ["Philip Chaney", "Korea, South", "Overland Park", "$38,735"],
-                ["Doris Greene", "Malawi", "Feldkirchen in Kärnten", "$63,542"],
-                ["Mason Porter", "Chile", "Gloucester", "$78,615"]
-              ]}
-            />
-          </CardBody>
-        </Card>
-      </GridItem>
-      <GridItem xs={12} sm={12} md={12}>
-        <Card plain>
-          <CardHeader plain color="primary">
-            <h4 className={classes.cardTitleWhite}>
-              Table on Plain Background
-            </h4>
-            <p className={classes.cardCategoryWhite}>
-              Here is a subtitle for this table
-            </p>
-          </CardHeader>
-          <CardBody>
-            <Table
-              tableHeaderColor="primary"
-              tableHead={["ID", "Name", "Country", "City", "Salary"]}
-              tableData={[
-                ["1", "Dakota Rice", "$36,738", "Niger", "Oud-Turnhout"],
-                ["2", "Minerva Hooper", "$23,789", "Curaçao", "Sinaai-Waas"],
-                ["3", "Sage Rodriguez", "$56,142", "Netherlands", "Baileux"],
-                [
-                  "4",
-                  "Philip Chaney",
-                  "$38,735",
-                  "Korea, South",
-                  "Overland Park"
-                ],
-                [
-                  "5",
-                  "Doris Greene",
-                  "$63,542",
-                  "Malawi",
-                  "Feldkirchen in Kärnten"
-                ],
-                ["6", "Mason Porter", "$78,615", "Chile", "Gloucester"]
-              ]}
+              tableHead={["Id", "Name"]}
+              tableData={playerItems}
             />
           </CardBody>
         </Card>
@@ -110,3 +98,5 @@ export default function Player() {
     </GridContainer>
   );
 }
+
+export default Player;
