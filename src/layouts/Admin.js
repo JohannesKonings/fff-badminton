35b6267efff9861463f741c5f2a98c39f905148e
Johannes Konings <johannes.konings@outlook.com>
2020-02-11 19:55:58 +0100

    renaming and logos

--- src/layouts/Admin.js
+++ src/layouts/Admin.js
@@ -16,7 +16,7 @@ import routes from "routes.js";
 import styles from "assets/jss/material-dashboard-react/layouts/adminStyle.js";
 
 import bgImage from "assets/img/sidebar-2.jpg";
-import logo from "assets/img/reactlogo.png";
+import logo from "assets/img/fff.png";
 
 import Amplify, { Auth, API } from 'aws-amplify';
 import { withAuthenticator } from 'aws-amplify-react';
@@ -101,7 +101,7 @@ function Admin({ ...rest }) {
     <div className={classes.wrapper}>
       <Sidebar
         routes={routes}
-        logoText={"Creative Tim"}
+        logoText={"fff badminton"}
         logo={logo}
         image={image}
         handleDrawerToggle={handleDrawerToggle}
