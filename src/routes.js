0e08f503b7b19f734c51faf80c26638051713121
Johannes Konings <johannes.konings@outlook.com>
2020-02-16 17:39:21 +0100

    nav notification deleted

--- src/routes.js
+++ src/routes.js
@@ -32,7 +32,6 @@ import Games from "views/Games/Games.js";
 import TableList from "views/TableList/TableList.js";
 import Typography from "views/Typography/Typography.js";
 import Icons from "views/Icons/Icons.js";
-import NotificationsPage from "views/Notifications/Notifications.js";
 import UpgradeToPro from "views/UpgradeToPro/UpgradeToPro.js";
 
 const dashboardRoutes = [
@@ -83,13 +82,6 @@ const dashboardRoutes = [
     name: "Icons",
     icon: BubbleChart,
     component: Icons,
-    layout: "/admin"
-  },
-  {
-    path: "/notifications",
-    name: "Notifications",
-    icon: Notifications,
-    component: NotificationsPage,
     layout: "/admin"
   },
   {
