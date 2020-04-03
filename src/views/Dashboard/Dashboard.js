606d569d9984a568e71d7255ae97d41604e76213
Johannes Konings <johannes.konings@outlook.com>
2020-04-03 20:21:47 +0200

    short facts for dashboard added (#45)
    https://github.com/JohannesKonings/fff-badminton/issues/27

--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from "react";
 // @material-ui/core
 import { makeStyles } from "@material-ui/core/styles";
+import Icon from "@material-ui/core/Icon";
+import Warning from "@material-ui/icons/Warning";
+import Store from "@material-ui/icons/Store";
 // core components
 import GridItem from "components/Grid/GridItem.js";
 import GridContainer from "components/Grid/GridContainer.js";
@@ -8,6 +11,9 @@ import Table from "components/Table/Table.js";
 import Card from "components/Card/Card.js";
 import CardHeader from "components/Card/CardHeader.js";
 import CardBody from "components/Card/CardBody.js";
+import CardIcon from "components/Card/CardIcon.js";
+import CardFooter from "components/Card/CardFooter.js";
+import Danger from "components/Typography/Danger.js";
 
 import styles from "assets/jss/material-dashboard-react/views/dashboardStyle.js";
 
@@ -26,21 +32,75 @@ export default function Dashboard() {
   }, []);
 
   const onPageRendered = async () => {
-    createTrainingsList();
+    readAllGames();
   };
 
+  const [allGamesItems, setGamesItems] = useState([]);
   const [trainingsItems, setTrainingItems] = useState([]);
+  const [numberOfGameDays, setNumberOfGameDays] = useState();
+  const [numberOfGames, setNumberOfGames] = useState();
+  const [averageParticipants, setAverageParticipants] = useState();
+  const [firstGameDayDate, setFirstGameDayDate] = useState();
+
+  const readAllGames = async () => {
+    const allGames = await API.graphql(graphqlOperation(listGames));
+    setGamesItems(allGames.data.listGames.items);
+  };
+
+  useEffect(() => {
+    createTrainingsList();
+    getNumberOfGameDays();
+    getNumberOfGames();
+    getAverageParticipants();
+  }, [allGamesItems]);
+
+  function countUnique(iterable) {
+    return new Set(iterable).size;
+  }
+
+  const getNumberOfGameDays = async () => {
+    const gameDayList = allGamesItems.flatMap(item => [item.gameday.id]);
+    setNumberOfGameDays(countUnique(gameDayList));
+
+    const sortedDates = gameDayList.sort();
+    const minDate = sortedDates[0];
+    setFirstGameDayDate(minDate);
+  };
+
+  const getNumberOfGames = async () => {
+    setNumberOfGames(Object.keys(allGamesItems).length);
+  };
+  const getAverageParticipants = async () => {
+    let gameDayPlayerCombined = [];
+
+    let gameDayPlayer1 = allGamesItems.map(item => {
+      return [item.gameday.id, item.player1.id];
+    });
+    let gameDayPlayer2 = allGamesItems.map(item => {
+      return [item.gameday.id, item.player2.id];
+    });
+    gameDayPlayerCombined.push(...gameDayPlayer1);
+    gameDayPlayerCombined.push(...gameDayPlayer2);
+
+    console.log(gameDayPlayerCombined);
+
+    let set = new Set(gameDayPlayerCombined.map(JSON.stringify));
+    let gameDayPlayerCombinedUnique = Array.from(set).map(JSON.parse);
+
+    const gameDayList = gameDayPlayerCombined.flatMap(item => [item[0]]);
+    let numberOfGameDays = countUnique(gameDayList);
+
+    if (numberOfGameDays !== 0) {
+      let averageParticipants = gameDayPlayerCombinedUnique.length / numberOfGameDays;
+      setAverageParticipants(averageParticipants.toFixed(2));
+    }
+  };
 
   const createTrainingsList = async () => {
-    const allGames = await API.graphql(graphqlOperation(listGames));
-
-    const allGamesItems = allGames.data.listGames.items;
-
     const gamesList = allGamesItems.flatMap(item => [
       item.player1.name,
       item.player2.name
     ]);
-
     var gamesListNo = [];
     gamesList.reduce(function(res, value) {
       if (!res[value]) {
@@ -63,6 +123,51 @@ export default function Dashboard() {
   const classes = useStyles();
   return (
     <div>
+      <GridContainer>
+        <GridItem xs={12} sm={6} md={3}>
+          <Card>
+            <CardHeader color="warning" stats icon>
+              <CardIcon color="warning">
+                <Icon>content_copy</Icon>
+              </CardIcon>
+              <p className={classes.cardCategory}>number gamedays</p>
+              <h3 className={classes.cardTitle}>{numberOfGameDays}</h3>
+            </CardHeader>
+            <CardFooter stats>
+              <div className={classes.stats}>
+                <Danger>
+                  <Warning />
+                </Danger>
+                <a href="#pablo" onClick={e => e.preventDefault()}>
+                  Since {firstGameDayDate}
+                </a>
+              </div>
+            </CardFooter>
+          </Card>
+        </GridItem>
+        <GridItem xs={12} sm={6} md={3}>
+          <Card>
+            <CardHeader color="success" stats icon>
+              <CardIcon color="success">
+                <Store />
+              </CardIcon>
+              <p className={classes.cardCategory}>number of games</p>
+              <h3 className={classes.cardTitle}>{numberOfGames}</h3>
+            </CardHeader>
+          </Card>
+        </GridItem>
+        <GridItem xs={12} sm={6} md={3}>
+          <Card>
+            <CardHeader color="danger" stats icon>
+              <CardIcon color="danger">
+                <Icon>info_outline</Icon>
+              </CardIcon>
+              <p className={classes.cardCategory}>average participants</p>
+              <h3 className={classes.cardTitle}>{averageParticipants}</h3>
+            </CardHeader>
+          </Card>
+        </GridItem>
+      </GridContainer>
       <GridContainer>
         <GridItem xs={12} sm={12} md={6}>
           <Card>
