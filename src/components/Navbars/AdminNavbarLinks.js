816d9c7f83df4d71821c84699253e82d2bb058ed
Johannes Konings <johannes.konings@outlook.com>
2020-06-11 15:04:31 +0200

    fix: unused variable removed

--- src/components/Navbars/AdminNavbarLinks.js
+++ src/components/Navbars/AdminNavbarLinks.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Link, useHistory } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import classNames from "classnames";
 // @material-ui/core components
 import { makeStyles } from "@material-ui/core/styles";
